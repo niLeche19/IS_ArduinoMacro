346c4d150c4489fe3135c3863edacf660a016d97
niLeche19 <42844776+niLeche19@users.noreply.github.com>
2020-03-30 19:48:40 -0400

    working version

--- VersionOne/main.ino
+++ VersionOne/main.ino
@@ -1,9 +1,6 @@
-
-//#include "Keyboard.h"
+#include "HID-Project.h"
 #include "keys.h"
 
-int zdown = 0;
-int xdown = 0;
 int debounce = 10;
 int klen = sizeof(KEYS)/4;
 
@@ -13,34 +10,48 @@ void setup() {
   }
   pinMode(5, OUTPUT);
   Keyboard.begin();
+  Consumer.begin();
   Serial.begin(9600);
 }
+
+//================================================================================
 // This check if the key pressed is a consumer code; returns true or false accordingly
-/*
 bool isCons(int key){
-  for(int l = (sizeof(CONS) / 4) - 1; l >= 0; l --){
+  for(int l = (sizeof(CONS) / 2) - 1; l >= 0; l --){
     if(key == CONS[l]){
       return(true);
     }
   }
   return(false);
 }
-// 
-*/
+
+//================================================================================
+// This either presses or releases the key 'k' depending on the value of 'pr'
 void keyAction(int k, bool pr){
   for(int i = 0; i < 10; i ++){
-    int kc = ASSMENTS[k][i];
-    if(pr == 1){
-      Keyboard.press(kc);
+    int kc = ConsumerKeycode(ASSMENTS[k][i]);
+    if(kc != 0){
+    }
+    if(pr == 1 and kc != 0){
+      if(isCons(kc)){
+        Consumer.press(kc);
+      }else{
+        Keyboard.press(KeyboardKeycode(kc));
+      }
     }else{
-      Keyboard.release(kc);
+      if(isCons(kc)){
+        Consumer.release(kc);
+      }else{
+        Keyboard.release(KeyboardKeycode(kc));
+      }
     }
   }
 }
 
-void loop() {  
+void loop() {
   for(int i = klen - 1; i >= 0; i --){
     if(digitalRead(KEYS[i][0]) == LOW and KEYS[i][1] == 0){
+      //Serial.println((sizeof(CONS) / 4) - 1);
       KEYS[i][1] = 1;
       keyAction(i, true);
       delay(debounce);
